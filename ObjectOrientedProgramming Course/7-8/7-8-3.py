c91796287287018eb292b618c92035d3ff6131fa
Timur Shamsrakhmanov <t.shamsrakhmanov@gmail.com>
2024-07-10 22:25:50 +0400

    commit

--- ObjectOrientedProgramming Course/7-8/7-8-3.py
+++ ObjectOrientedProgramming Course/7-8/7-8-3.py
@@ -1,8 +1,6 @@
 class Card:
 
     def __init__(self, suit, rank):
-        suit_db = {'♣', '♢', '♡', '♠'}
-        rank_db = {'2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'}
         self.suit = suit
         self.rank = rank
 

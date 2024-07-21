aba2765aad1a0d3fdae0bd7e3d6fd7b5d5e6ced7
Timur Shamsrakhmanov <t.shamsrakhmanov@gmail.com>
2024-07-21 09:32:48 +0400

    commit

--- ObjectOrientedProgramming Course/8-2/8-2-3.py
+++ ObjectOrientedProgramming Course/8-2/8-2-3.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from datetime import date
+from dateutil.relativedelta import relativedelta
 
 
 class Weekday(Enum):
@@ -12,6 +14,52 @@ class Weekday(Enum):
 
 
 class NextDate:
-    from datetime import date
-    def __init__(self, today: date, weekday:Weekday, after_today=False):
-        pass
+
+    def __init__(self, today: date, weekday: Weekday, considering_today=False):
+        self.today = today
+        self.weekday = weekday
+        self.considering_today = considering_today
+        self.counter = 0
+
+    def date(self):
+
+        if self.today.weekday() != self.weekday.value:
+
+            counter = 0
+            temp_date = self.today
+            while ...:
+                temp_date += relativedelta(days=1)
+                counter += 1
+
+                if temp_date.weekday() == self.weekday.value:
+                    self.counter = counter
+                    break
+
+            return temp_date
+
+
+        else:
+
+            # если учитываем "сегодня" и эти дни и так уже равны - то дельта (каунтер) == 0, и возвращаем "сегондя
+            if self.considering_today and self.today.weekday() == self.weekday.value:
+                self.counter = 0
+                return self.today
+
+            # если учитываем "сегодня" но эти дни не равны, то "отсчитываем от сегодняшней даты и идём
+            # в бесконечность, пока не найдём weekday
+            if not self.considering_today:
+                counter = 0
+                temp_date = self.today
+                while ...:
+                    temp_date += relativedelta(days=1)
+
+                    counter += 1
+
+                    if temp_date.weekday() == self.weekday.value:
+                        self.counter = counter
+                        break
+
+                return temp_date
+
+    def days_until(self):
+        return self.counter
